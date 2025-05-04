e0ab4ade98e4fe5e308f6135340f63a4ac21cb72
E M Thornber <enchanted.systems@btinternet.com>
2025-05-04 14:55:22 +0100

    Added version string code generated from project version string.

---
+++ src/canpi_server_version.h
@@ -0,0 +1,8 @@
+// from "Professional CMake: A Practical Guide" by Craig Scott
+#include <string>
+
+std::string getCanpiServerVersion();
+unsigned getCanpiServerVersionMajor();
+unsigned getCanpiServerVersionMinor();
+unsigned getCanpiServerVersionPatch();
+unsigned getCanpiServerVersionTweak();
